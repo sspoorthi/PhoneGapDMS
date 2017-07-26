d846db76e97cca9d854ba9c5bdd6f3c977e34fb6
sspoorthi <spoorthi.sham@philips.com>
2017-07-26 10:09:28 +0530

    Notification on Incident report

--- www/app/components/controllers/homeController.js
+++ www/app/components/controllers/homeController.js
@@ -2,6 +2,7 @@ export   function homeController($scope,$location,appFactory,appService) {
 "ngInject";
     $scope.populateRules = populateRules;
     $scope.showIncidentForm = showIncidentForm;
+    $scope.reportIncident = reportIncident;
     $scope.appService=appService;
     function populateRules(){
         appFactory.getRules().then(res=>{
@@ -20,5 +21,26 @@ export   function homeController($scope,$location,appFactory,appService) {
 
     function showIncidentForm() {
          $location.path('/IncidentForm');
+    }
+
+    function reportIncident() {
+        $scope.appService.incident = $scope.incident;
+        $scope.appService.incidentDescription = $scope.incidentDescription;
+        $scope.appService.incidentLocation = $scope.incidentLocation;
+        if($scope.appService.userRole == "admin") {
+            var title = "ALERT";
+            var message = "Incident: " + $scope.appService.incident + "\n Incident Description: " + $scope.appService.incidentDescription 
+                + "\n Incident location:" + $scope.appService.incidentLocation;
+            var buttonName = "Acknowledge";
+            navigator.notification.alert(message, ack, title, buttonName);
+            var times = 3;
+            navigator.notification.beep(times);
+
+            function ack() {
+                console.log("Acknowledged!");
+                $location.path('/Rules');
+            }
+        }
+        
     }
 }
